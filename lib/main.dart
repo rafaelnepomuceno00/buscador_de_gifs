fc136261009e7ed7bcd13cbb47850c888d2c3b2d
Rafael <rafaelenepomuceno00@gmail.com>
2020-07-28 03:57:40 -0300

    Initial commit
    -app completo
    
    -api funcionando

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:buscadordegifs/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:buscadordegifs/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
+  ));
+}
+
+
+
+
